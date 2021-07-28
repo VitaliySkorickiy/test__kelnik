b1a93ea63af6676a2e2f9b68297c2eda6d7a5582
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-07-28 14:53:08 +0300

    вёрстка 1440

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
-$(function(){
 
+console.log(1);
 
+// $(".js-range-slider").ionRangeSlider({
+//   type: "double",
+//   grid: true,
+//   min: 1000000,
+//   max: 20000000,
+//   from: -500,
+//   to: 500
+// });
 
-  
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 100000,
+  max: 30000000,
+  from: 1
 });

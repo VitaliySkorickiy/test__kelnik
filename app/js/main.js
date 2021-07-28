912ce19790413121fb44a0b44c6f454fa622a788
zarias2020 <62381691+zarias2020@users.noreply.github.com>
2021-07-28 19:19:25 +0300

    фильтер

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,47 @@
 
 console.log(1);
 
-// $(".js-range-slider").ionRangeSlider({
-//   type: "double",
-//   grid: true,
-//   min: 1000000,
-//   max: 20000000,
-//   from: -500,
-//   to: 500
-// });
+const slider1 = document.getElementById('range-slider1');
+const slider2 = document.getElementById('range-slider2');
+const minPrice = document.querySelector('.min-price');
+const maxPrice = document.querySelector('.max-price');
+const minSpace = document.querySelector('.min-space');
+const maxSpace = document.querySelector('.max-space');
 
-$(".js-range-slider").ionRangeSlider({
-  type: "double",
-  min: 100000,
-  max: 30000000,
-  from: 1
-});
+const hsowSlidersValues = (slider, minDiv, maxDiv) => {
+
+  slider.noUiSlider.on('update', (values) => {
+    let minValues = Math.round(values[0]).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    minDiv.querySelector('span').textContent = minValues;
+
+    let maxValues = Math.round(values[1]).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    maxDiv.querySelector('span').textContent = maxValues;
+  })
+}
+
+if (slider1) {
+  noUiSlider.create(slider1, {
+    start: [5500000, 18900000],
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 35000000
+    }
+  });
+
+  hsowSlidersValues(slider1, minPrice, maxPrice);
+}
+
+if (slider2) {
+  noUiSlider.create(slider2, {
+    start: [33, 123],
+    connect: true,
+    range: {
+      'min': 10,
+      'max': 220
+    }
+  });
+
+  hsowSlidersValues(slider2, minSpace, maxSpace);
+}
+
